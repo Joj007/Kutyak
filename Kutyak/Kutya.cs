725a4a8c46bb2c3ae0ca382370572ddfd9d23d11
Joj007 <sinkajozsef2005@gmail.com>
2023-05-25 13:00:02 +0200

    Készen van

---
+++ Kutyak/Kutya.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Linq;
+
+namespace Kutyak
+{
+    internal class Kutya
+    {
+        int foId;
+        int fajtaId;
+        int nevId;
+        int eletkor;
+        DateTime utolsoOrvosiEllenorzes;
+        string magyarFajta;
+        string angolFajta;
+        string nev;
+
+        public Kutya(string[] adatok)
+        {
+            this.foId = int.Parse(adatok[0]);
+            this.fajtaId = int.Parse(adatok[1]);
+            this.nevId = int.Parse(adatok[2]);
+            this.eletkor = int.Parse(adatok[3]);
+            string[] datumSeged = adatok[4].Split('.');
+            this.utolsoOrvosiEllenorzes = new DateTime(int.Parse(datumSeged[0]), int.Parse(datumSeged[1]), int.Parse(datumSeged[2]));
+
+            this.nev = File.ReadAllLines("kutyaNevek.csv").Skip(1).Where(n => n.Split(';')[0] == $"{nevId}").Select(n => n.Split(';')[1]).First();
+            this.magyarFajta = File.ReadAllLines("kutyaFajtak.csv").Skip(1).Where(n => n.Split(';')[0] == $"{fajtaId}").Select(n => n.Split(';')[1]).First();
+            this.angolFajta = File.ReadAllLines("kutyaFajtak.csv").Skip(1).Where(n => n.Split(';')[0] == $"{fajtaId}").Select(n => n.Split(';')[2]).First();
+
+        }
+
+        public int FoId { get => foId;}
+        public int FajtaId { get => fajtaId;}
+        public int NevId { get => nevId;}
+        public int Eletkor { get => eletkor;}
+        public DateTime UtolsoOrvosiEllenorzes { get => utolsoOrvosiEllenorzes;}
+        public string MagyarFajta { get => magyarFajta;}
+        public string AngolFajta { get => angolFajta;}
+        public string Nev { get => nev;}
+
+        public string nevEsFajta { get => $"{nev};{MagyarFajta}";}
+
+        
+
+    }
+}
--- Kutyak/Program.cs
+++ Kutyak/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Kutyak
 {
@@ -10,6 +11,16 @@ namespace Kutyak
     {
         static void Main(string[] args)
         {
+            List<Kutya> kutyak = new List<Kutya>();
+            File.ReadAllLines("Kutyak.csv").Skip(1).ToList().ForEach(n => kutyak.Add(new Kutya(n.Split(';'))));
+            Console.WriteLine($"2. feladat: Kutyanevek száma: {File.ReadAllLines("KutyaNevek.csv").Skip(1).Count()}");
+            Console.WriteLine($"6. feladat: Kutyák átlag életkora: {Math.Round(kutyak.Average(n => n.Eletkor), 2)}");
+            Console.WriteLine($"7. feladat: Legidősebb kutya neve és fajtája: {kutyak.Where(n => n.Eletkor == (kutyak.Max(m => m.Eletkor))).First().nevEsFajta}");
+            Console.WriteLine("8. feladat: Január 10.-én vizsgált kutya fajták: ");
+            kutyak.Where(n => n.UtolsoOrvosiEllenorzes == new DateTime(2018, 01, 10)).GroupBy(n => n.MagyarFajta).ToList().ForEach(n => Console.WriteLine($"\t{n.Key}: {n.Count()} kutya"));
+            Console.WriteLine($"9. feladat: Legjobban leterhelt nap: {kutyak.GroupBy(n => n.UtolsoOrvosiEllenorzes).Where(n=>n.Count()== kutyak.GroupBy(m => m.UtolsoOrvosiEllenorzes).Max(m => m.Count())).First().Key.ToShortDateString()}: {kutyak.GroupBy(n => n.UtolsoOrvosiEllenorzes).Max(n=>n.Count())} kutya");
+            Console.WriteLine("10. feladat: névstatisztika.txt");
+            File.WriteAllLines("Névstatisztika.txt" ,kutyak.GroupBy(n=>n.Nev).OrderByDescending(n=>n.Count()).Select(n=>$"{n.Key}, {n.Count()}").ToArray());
         }
     }
 }
